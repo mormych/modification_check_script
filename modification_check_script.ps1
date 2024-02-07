dd1c6c24755d5ff39f4538998522ce5dcc277733
Michał Adamiak <71809600+mormych@users.noreply.github.com>
2024-02-07 11:30:35 +0100

    Update
    Dodana obsługa zewnętrznego pliku config.cfg do konfiguracji działania skryptu

--- modification_check_script.ps1
+++ modification_check_script.ps1
@@ -1,11 +1,25 @@
 ﻿# Skrypt sprawdza czy plik uległ modyfikacji
+function Setup-Config () {
+    If( -Not ((Get-Content -Path .\config.cfg -ReadCount 1) -eq "[modification_check_script]"))
+    {
+        exit 1
+    }
 
-Param (
-    $CurrentDate = $(Get-Date -Format("dd.MM.yyyy HH:mm:ss")),
-    [Int32]$NoModifyTimeAllowed = 1, #In minutes 
-    $DirToCheck = "",
-    [string[]]$AllowedExtension = ('*.txt', '*.rtf', '*.doc', '*.csv')
-)
+    Get-Content -Path .\config.cfg | ForEach-Object -Begin {$config = @{}} -Process {$data = [regex]::Split($_, "="); If(($data[0].StartsWith("[") -ne $True) -and ($data[0].CompareTo("") -ne 0)) {$config.Add($data[0], $data[1])}}
+    #General
+    $global:daysToCheck = $config.Get_Item("daysToCheck") -split ",";
+    $global:dirsToCheck = $config.Get_Item("dirsToCheck") -split ",";
+    $global:noModifyTime = $config.Get_Item("noModifyTime");
+    $global:excludedFormats = $config.Get_Item("excludedFormats") -split ",";
+    $global:CurrentDate = $(Get-Date -Format("dd.MM.yyyy HH:mm:ss"));
+    #Sending
+    $global:port = $config.Get_Item("port");
+    $global:smtp = $config.Get_Item("smtp");
+    $global:user = $config.Get_Item("user");
+    $global:pass = $config.Get_Item("pass");
+    $global:destination = $config.Get_Item("destination") -split ",";
+
+}
 
 function Read-File {
     param (
@@ -22,23 +36,14 @@ function Send-Email() {
         [Int32]$LastTimeEdit
     )
 
-    # Pobieram adresy dla ktorych wysle maila
-    $addresses = Read-File -FileName $PSScriptRoot\addresses
-
-
-    # Pobieram dane dla serwera smtp
-    $config = Read-File -FileName $PSScriptRoot\config
-    $username = $config[0]
-    $password = ConvertTo-SecureString $config[1] -AsPlainText -Force
-    $smtp = $config[2]
-
-    $Cred = New-Object System.Management.Automation.PSCredential ($username, $password)
+    $password = ConvertTo-SecureString $pass -AsPlainText -Force
+    $Cred = New-Object System.Management.Automation.PSCredential ($user, $password)
 
     $MailMessage = @{
-        To = $addresses
-        From = $username
+        To = $destination
+        From = $user
         Subject = "RCP skrypt"
-        Body = "<h1>Wykryto problem z plikiem</h1><p>Plik: <strong>$FileName</strong></p> <p>Nie został zaktualizowany w żądanym okresie czasowym</p><p>Jego ostatnia aktualizacja była <strong>$LastTimeEdit</strong> minut temu"
+        Body = "<h1>Wykryto problem z plikiem</h1><p>Plik: <strong>$FileName</strong></p> <p>Nie został zaktualizowany w żądanym okresie czasowym</p><p>Jego ostatnia aktualizacja była <strong>$LastTimeEdit</strong> minut temu (limit: <strong>$noModifyTime</strong>)"
         Smtpserver = $smtp
         BodyAsHtml = $true
         Encoding = "UTF8"
@@ -48,28 +53,27 @@ function Send-Email() {
     Send-MailMessage @MailMessage
 }
 
-If (-Not (Test-Path $DirToCheck)) {
-    Throw "Path -> '$DirToCheck' Not exist"
+Setup-Config
+
+If (-Not ($dirsToCheck | Test-Path)) {
+    Throw "Path -> '$dirsToCheck' Not exist"
 }
 
-Write-Host "Current date is: $CurrentDate"
-Write-Host
-Write-Host
+if(-Not ($daysToCheck -contains (Get-Date).DayOfWeek.value__))
+{
+    exit 2
+}
 
-Write-Host "Getting list of files in DIR $DirToCheck"
-Get-ChildItem -Path $DirToCheck -Recurse -Include $AllowedExtension
-$Files = Get-ChildItem -Path $DirToCheck -Recurse -Include $AllowedExtension
-Write-Host #Blank line
+foreach($dirToCheck in $dirsToCheck)
+{
+    Get-ChildItem -Path $dirToCheck -Recurse -Exclude $excludedFormats
+    $Files = Get-ChildItem -Path $dirToCheck -Recurse -Exclude $excludedFormats
 
-foreach ($File in $Files) {
-    $FileDate = $File.LastWriteTime
-    $FileLastTimeEditInMinutes = [Int32](NEW-TIMESPAN –Start $FileDate –End $CurrentDate).TotalMinutes
-    If ([Int32](NEW-TIMESPAN –Start $FileDate –End $CurrentDate).TotalMinutes -gt $NoModifyTimeAllowed) {
-        Write-Error "Warning! Plik: $($File.Name) nie został zmodyfikowany w żądanym okresie czasu"
-        Write-Error "Ostatnio zmodyfikowany $FileLastTimeEditInMinutes minut temu"
-        Send-Email -FileName $File.FullName -LastTimeEdit $FileLastTimeEditInMinutes
-        Write-Host
+    foreach ($File in $Files) {
+        $FileDate = $File.LastWriteTime
+        $FileLastTimeEditInMinutes = [Int32](NEW-TIMESPAN –Start $FileDate –End $CurrentDate).TotalMinutes
+        If ([Int32](NEW-TIMESPAN –Start $FileDate –End $CurrentDate).TotalMinutes -gt $noModifyTime) {
+            Send-Email -FileName $File.FullName -LastTimeEdit $FileLastTimeEditInMinutes
+        }
     }
 }
-
-Write-Host "Script done"
